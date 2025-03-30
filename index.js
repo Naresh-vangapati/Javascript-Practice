3c0d1aa0777e166ad107ce902a52048b51f224da
Naresh-vangapati <nareshvangapati1999@gmail.com>
2025-03-31 01:22:53 +0530

    image adding

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-let container = document.createElement('div');
-container.className = "container"
-let image = document.createElement("img");
-image.src = "https://images6.fanpop.com/image/photos/36600000/Tigers-image-tigers-36642865-1600-1200.jpg"
-let h1 = document.createElement("h1");
-h1.innerText = "Tiger image";
-let p = document.createElement("p");
-p.innerText = "Tiger is the greatest animal in the world";
-container.append(image,h1,p)
-document.body.appendChild(container)
+let container = document.getElementById("container");
+let image = ''
+container.innerHTML = `
+<img src = ${image}
+<h1>Hello world</h1>
+<p>tiger is the one of the greatest animal in the world`
