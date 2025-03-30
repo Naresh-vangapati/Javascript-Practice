5bda185eafded7240ac0b9d6564226b1fc46e381
Naresh-vangapati <nareshvangapati1999@gmail.com>
2025-03-31 01:33:06 +0530

    card creation with template literals

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let container = document.getElementById("container");
-let image = ''
+let image = 'https://images6.fanpop.com/image/photos/36600000/Tigers-image-tigers-36642865-1600-1200.jpg'
 container.innerHTML = `
-<img src = ${image}
-<h1>Hello world</h1>
-<p>tiger is the one of the greatest animal in the world`
+<img src = '${image}'>
+<h1 class = 'text'>Tiger Image</h1>
+<p>tiger is the one of the greatest animal in the world</p>`
